8c4d12d5b8e60806797b659e3bd50f46def0e4f8
bidinos2000 <69343027+bidinos2000@users.noreply.github.com>
2020-10-26 15:21:14 +0700

    update

--- src/components/about/aboutUs.tsx
+++ src/components/about/aboutUs.tsx
@@ -6,7 +6,7 @@ const AboutUs = () => {
             <div className="col-xl-6 col-lg-6 col-md-6">
                 <div className="about_thumb2">
                     <div className="img_1">
-                        <img src="img/about/1.png" alt=""/>
+                        <img src="img/about/about10.jpg" alt=""/>
                     </div>
                     <div className="img_2">
                         <img src="img/about/2.png" alt=""/>
@@ -23,7 +23,7 @@ const AboutUs = () => {
                     </div>
                     <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate</p>
                     <div className="img_thumb">
-                        <img src="img/jessica-signature.png" alt=""/>
+                        <img src="img/vung1.png" alt=""/>
                     </div>
                 </div>
             </div>
